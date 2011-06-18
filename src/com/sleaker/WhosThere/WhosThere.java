2240ce2afa6dace9e15e4a78926f1cb3edbf98aa
sleaker <sleaker@sleak-pc>
2011-06-17 17:41:38 -0700

    Update

--- src/com/sleaker/WhosThere/WhosThere.java
+++ src/com/sleaker/WhosThere/WhosThere.java
@@ -1,27 +1,22 @@
-package com.sleaker.MemManager;
+package com.sleaker.WhosThere;
 
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MemManager extends JavaPlugin{
+public class WhosThere extends JavaPlugin{
 
-	private final MemCheck memchecker = new MemCheck();
 	public static Logger log = Logger.getLogger("Minecraft");
-	public static final String plugName = "[MemManager]"; 
+	public static final String plugName = "[WhosThere]"; 
 
 	public void onDisable() {
 		log.info(plugName + " Disabled");
 	}
-
-
-
+	
 	public void onEnable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
 		log.info(plugName + " - " + pdfFile.getVersion() + " by Sleaker is enabled!");
-
-		getServer().getScheduler().scheduleSyncRepeatingTask(this, memchecker, 200, 6000);
 
 	}
 
